e095d7b732165f9b07f8f54c44b4bf3d404898d2
sydney miranda <sydmiranda@icloud.com>
2021-01-04 20:29:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ array_of_daily_temperatures = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-lowest_temp = 11
+lowest_temp = 100
 while element_index < src[row_index].count do
   if src[row_index][element_index] < lowest_temp
     lowest_temp = src[row_index][element_index]
@@ -14,6 +14,7 @@ element_index += 1
 end
 array_of_daily_temperatures << lowest_temp
 row_index += 1
+end
 end
 
 #find_min_in_nested_arrays(array_of_daily_temperatures)
