8ce03e0719a616bffb32e04cdb7ff41d131bd16d
sydney miranda <sydmiranda@icloud.com>
2021-01-04 20:21:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,8 @@ while row_index < src.count do
   element_index = 0
 lowest_temp = 0
 while element_index < src[row_index].count do
-  
+  if src[row_index][element_index] < lowest_temp
+    
 end
 end
 end
